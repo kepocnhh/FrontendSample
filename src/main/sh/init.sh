69e49de19580f3c051b5062b74cca364d9e42bac
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-05-10 15:35:25 +0300

    config

--- src/main/sh/init.sh
+++ src/main/sh/init.sh
@@ -18,33 +18,3 @@ git init \
 
 if test $? -ne 0; then
  echo 'Git init error!'; exit 1; fi
-
-VCS_API='https://api.github.com'
-
-ISSUER='/tmp/gh-user.json'
-CODE=$(curl -m 8 -w '%{http_code}' -o "${ISSUER}" \
- -H "Authorization: token ${VCS_PAT}" \
- "${VCS_API}/user")
-
-if test $? -ne 0; then
- echo 'Curl error!'; exit 1; fi
-
-if [[ "${CODE}" != '200' ]]; then
- echo 'Get user error!'; exit 1; fi
-
-if [[ ! -f "${ISSUER}" ]]; then
- echo "No file \"${ISSUER}\"!"; exit 1
-elif [[ ! -s "${ISSUER}" ]]; then
- echo "File \"${ISSUER}\" is empty!"; exit 1
-fi
-
-USER_NAME="$(yq -erM '.name // .login' "${ISSUER}")" || exit 1
-USER_ID="$(yq -erM .id "${ISSUER}")" || exit 1
-USER_LOGIN="$(yq -erM .login "${ISSUER}")" || exit 1
-USER_EMAIL="${USER_ID}+${USER_LOGIN}@users.noreply.github.com"
-
-git config user.name "${USER_NAME}" \
- && git config user.email "${USER_EMAIL}"
-
-if test $? -ne 0; then
- echo 'Git config error!'; exit 1; fi
