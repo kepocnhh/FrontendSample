bf0cbe01e020d5ff6e0f46e03157f8cc84af3602
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-05-15 17:28:17 +0300

    wip

--- src/main/sh/check_posts.sh
+++ src/main/sh/check_posts.sh
@@ -1,3 +1,21 @@
 #!/usr/local/bin/bash
 
+ISSUER='src/main/res/counts.bin'
+
+if [[ ! -f "${ISSUER}" ]]; then
+ echo "No file \"${ISSUER}\"!"; exit 1
+elif [[ ! -s "${ISSUER}" ]]; then
+ echo "File \"${ISSUER}\" is empty!"; exit 1
+elif [[ "$(wc -c < "${ISSUER}")" -ne 12 ]]; then
+ echo "File \"${ISSUER}\" size is not 12 bytes!"; exit 1
+fi
+
+COUNTS="$(xxd -p -c 12 "${ISSUER}")"
+PUBLISHED_COUNT=$((16#${COUNTS:0:8}))
+AWAITING_COUNT=$((16#${COUNTS:8:8}))
+COUNTER=$((16#${COUNTS:16:8}))
+
+if test ${AWAITING_COUNT} -eq 0; then
+ echo "No awaiting posts"; exit 0
+
 echo 'Not implemented!'; exit 1 # todo
